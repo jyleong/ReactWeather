2da9823c9aefb798f9c34145ea277b899fa22a7e
jameslNeurio <james.leong@neur.io>
2017-04-15 10:45:49 -0700

    stylgin for about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var About = React.createClass({
 //     render: function() {
@@ -13,7 +14,23 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h3>About component</h3>
+        <div>
+            <h1 className="text-center">About:</h1>
+            <p>Learning React.js by making a weather app!</p>
+            <p>
+            I am building this following along The Complete React Web App
+            Developer Course by Andrew Mead
+            </p>
+            <ul>
+                <li>
+                    <a href="https://github.com/jyleong">My github</a>
+                </li>
+                <li>
+                    <a href="https://facebook.github.io/react">ReactM</a> - This was the 
+                    JavaScript framework used.
+                </li>
+            </ul>
+        </div>
     );
 };
 
