676d8b8bfef5725b7e2c95c236496bed3383d9e2
jameslNeurio <james.leong@neur.io>
2017-04-15 12:48:02 -0700

    error modal functional

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,18 +1,28 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+    getDefaultProps: function() {
+        return {
+            title: 'Error'
+        };  
+    },
+    propTypes: {
+        title: React.PropTypes.string,
+        message: React.PropTypes.string.isRequired
+    },
     componentDidMount: function() {
         //create new instance of foundation reveal
         var modal = new Foundation.Reveal($('#error-modal'));
         modal.open();
     },
     render: function() {
+        var {title, message} = this.props;
         // the foundation class for modal
         //data-reveal lets foundation we are using a modal
         return (
             <div id="error-modal" className="tiny reveal text-center" data-reveal="">
-                <h4>Some Title</h4>
-                <p>Our current message</p>
+                <h4>{title}</h4>
+                <p>Error: {message}</p>
                 <button className="button hollow" data-close="">
                 Okay
                 </button>
